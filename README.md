1587776866fa13d8cc7d5dbb4869b5362243acbd
Aerendir <hello@aerendir.me>
2016-05-08 14:19:00 +0200

    Add `ArrayWriter`.

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
+[![Latest Stable Version](https://poser.pugx.org/serendipity_hq/php_array_writer/v/stable.png)](https://packagist.org/packages/serendipity_hq/php_array_writer)
+[![Build Status](https://travis-ci.org/Aerendir/PHPArrayWriter.svg?branch=master)](https://travis-ci.org/Aerendir/PHPArrayWriter)
+[![Total Downloads](https://poser.pugx.org/serendipity_hq/php_array_writer/downloads.svg)](https://packagist.org/packages/serendipity_hq/php_array_writer)
+[![License](https://poser.pugx.org/serendipity_hq/php_array_writer/license.svg)](https://packagist.org/packages/serendipity_hq/php_array_writer)
+[![Code Climate](https://codeclimate.com/github/Aerendir/PHPArrayWriter/badges/gpa.svg)](https://codeclimate.com/github/Aerendir/PHPArrayWriter)
+[![Test Coverage](https://codeclimate.com/github/Aerendir/PHPArrayWriter/badges/coverage.svg)](https://codeclimate.com/github/Aerendir/PHPArrayWriter)
+[![Issue Count](https://codeclimate.com/github/Aerendir/PHPArrayWriter/badges/issue_count.svg)](https://codeclimate.com/github/Aerendir/PHPArrayWriter)
+[![StyleCI](https://styleci.io/repos/38658138/shield)](https://styleci.io/repos/38658138)
+[![SensioLabsInsight](https://insight.sensiolabs.com/projects/d31e356d-d01d-4b1b-990e-a1fa46acb050/mini.png)](https://insight.sensiolabs.com/projects/d31e356d-d01d-4b1b-990e-a1fa46acb050)
+[![Dependency Status](https://www.versioneye.com/user/projects/572f442aa0ca3500508408c9/badge.svg?style=flat)](https://www.versioneye.com/user/projects/572f442aa0ca3500508408c9)
+
 # PHPArrayWriter
 A class to write arrays
---
+++ tests/ArrayWriterTest.php
@@ -0,0 +1,327 @@
+<?php
+
+/**
+ *    @author      Aerendir <hello@aerendir.me>
+ *    @copyright   Copyright (C) 2014 SerendipityHQ. All rights reserved.
+ */
+namespace SHQ\Component\ArrayWriter\Tests;
+
+use SHQ\Component\ArrayWriter\ArrayWriter;
+use Symfony\Component\PropertyAccess\Exception\AccessException;
+
+/**
+ * Class ArrayWriterTest.
+ */
+class ArrayWriterTest extends \PHPUnit_Framework_TestCase
+{
+    /** @var  ArrayWriter $resource */
+    private $resource;
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function setUp()
+    {
+        $this->resource = new ArrayWriter();
+    }
+
+    public function testGetValue()
+    {
+        $testArray = [
+            'level1' => [
+                'value 1', 'value 2', 'value 3'
+            ]
+        ];
+        
+        $this->assertSame($testArray['level1'], $this->resource->getValue($testArray, '[level1]'));
+    }
+
+    public function testGetValueReturnsEntireArrayIfPathIsEmpty()
+    {
+        $testArray = [
+            'level1' => [
+                'value 1', 'value 2', 'value 3'
+            ]
+        ];
+        $this->assertSame($testArray, $this->resource->getValue($testArray, ''));
+    }
+
+    public function testIsNode()
+    {
+        $testArray = [
+            'level1' => [
+                'value 1', 'value 2', 'value 3'
+            ]
+        ];
+        $this->assertSame(true, $this->resource->isNode($testArray, '[level1]'));
+    }
+    
+    public function testAdd()
+    {
+        $testArray = [
+            'level1' => [
+                'value 1', 'value 2', 'value 3'
+            ]
+        ];
+        
+        $expected = [
+            'level1' => [
+                'value 1', 'value 2', 'value 3', 'ciao'
+            ]
+        ];
+        
+        $this->resource->add($testArray, '[level1]', 'ciao');
+        $this->assertSame($expected, $testArray);
+    }
+
+    public function testAddPreservesAlreadyExistentValueInToPath()
+    {
+        $testArray = [
+            'level1' => [
+                'value 1', 'value 2', 'value 3'
+            ]
+        ];
+        
+        $expected = [
+            'level1' => [
+                0 => [
+                    'value 1', 'ciao'
+                ],
+                'value 2',
+                'value 3'
+            ]
+        ];
+
+        $this->resource->add($testArray, '[level1][0]', 'ciao');
+        $this->assertSame($expected, $testArray);
+    }
+
+    public function testAddCanSetPropertyNameForOldAndNewValues()
+    {
+        $testArray = [
+            'level1' => [
+                'value 1', 'value 2', 'value 3'
+            ]
+        ];
+        
+        $expected = [
+            'level1' => [
+                0 => [
+                    'OldValue' => 'value 1',
+                    'NewValue' => 'ciao'
+                ],
+                'value 2',
+                'value 3'
+            ]
+        ];
+
+        $this->resource->add($testArray, '[level1][0]', 'ciao', 'NewValue', 'OldValue');
+        $this->assertSame($expected, $testArray);
+    }
+
+    public function testCp()
+    {
+        $testArray = [
+            'level1' => [
+                'value 1', 'value 2', 'value 3'
+            ]
+        ];
+        
+        $result = [
+            'level1' => [
+                'value 1', 'value 2', 'value 3'
+            ],
+            'level2' => [
+                'value 1', 'value 2', 'value 3'
+            ]
+        ];
+        $this->resource->cp($testArray, '[level1]', '[level2]');
+        $this->assertSame($result, $testArray);
+    }
+
+    public function testCpThrowsExceptionIfFromIsNotReadable()
+    {
+        $testArray = [
+            'level1' => [
+                'value 1', 'value 2', 'value 3'
+            ]
+        ];
+        $this->expectException(AccessException::class);
+        $this->resource->cp($testArray, '[not-known]', '[not-known2]');
+    }
+
+    public function testCpSafe()
+    {
+        $testArray = [
+            'level1' => [
+                'value 1', 'value 2', 'value 3'
+            ]
+        ];
+        $result = [
+            'level1' => [
+                'value 1', 'value 2', 'value 3'
+            ],
+            'level2' => [
+                'value 1', 'value 2', 'value 3'
+            ]
+        ];
+        $this->resource->cpSafe($testArray, '[level1]', '[level3]');
+        $this->assertSame($result['level1'], $testArray['level3']);
+    }
+    
+    /**
+     * This exception is not testable as the isWrite method ever returns true.
+     */
+    public function testCpSafeThrowsExceptionIfToIsNotWritable()
+    {
+        $test = [
+            'level1' => [
+                'value 1.1', 'value 1.2', 'value 1.3'
+            ],
+            'level2' => [
+                'value 2.1', 'value 2.2', 'value 2.3'
+            ]
+        ];
+        $this->expectException(AccessException::class);
+        $this->resource->cpSafe($test, '[level1]', '[level2]');
+    }
+
+    public function testEdit()
+    {
+        $test = [
+            'level1' => [
+                'value 1.1', 'value 1.2', 'value 1.3'
+            ]
+        ];
+        $result = [
+            'value 2.1', 'value 2.2', 'value 2.3'
+        ];
+        
+        $this->resource->edit($test, '[level1]', $result);
+        $this->assertSame($test['level1'], $result);
+    }
+
+    public function testEditRecognizeRoot()
+    {
+        $test = [
+            'level1' => [
+                'value 1.1', 'value 1.2', 'value 1.3'
+            ]
+        ];
+        $result = [
+            'value 2.1', 'value 2.2', 'value 2.3'
+        ];
+
+        $this->resource->edit($test, '[]', $result);
+        $this->assertSame($test, $result);
+    }
+
+    public function testEditThrowsExceptionIfPathDoesntExist()
+    {
+        $testArray = [
+            'level1' => [
+                'value 1', 'value 2', 'value 3'
+            ]
+        ];
+        $this->expectException(AccessException::class);
+        $this->resource->edit($testArray, '[non-existent]', ['value 1', 'value 2']);
+    }
+    
+    public function testMv()
+    {
+        $testArray = [
+            'level1' => [
+                'value 1', 'value 2', 'value 3'
+            ]
+        ];
+        $result = [
+            'level2' => [
+                'value 1', 'value 2', 'value 3'
+            ]
+        ];
+        $this->resource->mv($testArray, '[level1]', '[level2]');
+        $this->assertSame($result, $testArray);
+    }
+
+    public function testMvSafe()
+    {
+        $testArray = [
+            'level1' => [
+                'value 1', 'value 2', 'value 3'
+            ]
+        ];
+        $result = [
+            'level2' => [
+                'value 1', 'value 2', 'value 3'
+            ]
+        ];
+        $this->resource->mvSafe($testArray, '[level1]', '[level2]');
+        $this->assertSame($result, $testArray);
+    }
+
+    public function testMvSafeThrowsExceptionIfToIsNotWritable()
+    {
+        $test = [
+            'level1' => [
+                'value 1.1', 'value 1.2', 'value 1.3'
+            ],
+            'level2' => [
+                'value 2.1', 'value 2.2', 'value 2.3'
+            ]
+        ];
+        $this->expectException(AccessException::class);
+        $this->resource->mvSafe($test, '[level1]', '[level2]');
+    }
+    
+    public function testRm()
+    {
+        $testArray = [
+            'level1' => [
+                'value 1', 'value 2', 'value 3'
+            ]
+        ];
+        $result = ['level1' => ['value 1', 'value 2']];
+        $this->resource->rm($testArray, '[level1][2]');
+        $this->assertSame($result, $testArray);
+    }
+    
+    public function testWrap()
+    {
+        $testArray = [
+            'level1' => [
+                'value 1', 'value 2', 'value 3'
+            ]
+        ];
+        
+        $result = [
+            'level1' => [
+                'wrapper' => [
+                    'value 1', 'value 2', 'value 3'
+                ]
+            ]
+        ];
+        
+        $this->resource->wrap($testArray, '[level1]', 'wrapper');
+        $this->assertSame($result, $testArray);
+    }
+
+    public function testWrapRoot()
+    {
+        $testArray = [
+            'level1' => [
+                'value 1', 'value 2', 'value 3'
+            ]
+        ];
+
+        $result = [
+            'wrapper' => [
+                'level1' => [
+                    'value 1', 'value 2', 'value 3'
+                ]
+            ]
+        ];
+
+        $this->resource->wrap($testArray, '', 'wrapper');
+        $this->assertSame($result, $testArray);
+    }
+}
