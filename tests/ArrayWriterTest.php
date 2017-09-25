1fcc654664447f4e11fb56e8d2ec6c3d73eb7c34
Aerendir <hello@aerendir.me>
2017-09-25 19:03:09 +0200

    Apply style fixes.
    Stick composer versions.

--- tests/ArrayWriterTest.php
+++ tests/ArrayWriterTest.php
@@ -1,22 +1,31 @@
 <?php
 
-/**
- *    @author      Aerendir <hello@aerendir.me>
- *    @copyright   Copyright (C) 2014 SerendipityHQ. All rights reserved.
+/*
+ * This file is part of the PHP Array Writer Component.
+ *
+ * Copyright Adamo Aerendir Crespi 2014-2017.
+ *
+ * See the LICENSE for more details.
+ *
+ * @author    Adamo Aerendir Crespi <hello@aerendir.me>
+ * @copyright Copyright (C) 2014 - 2017 Aerendir. All rights reserved.
+ * @license   MIT License
  */
+
 namespace SHQ\Component\ArrayWriter\Tests;
 
+use PHPUnit\Framework\TestCase;
 use SHQ\Component\ArrayWriter\ArrayWriter;
 use Symfony\Component\PropertyAccess\Exception\AccessException;
 
 /**
  * Class ArrayWriterTest.
  */
-class ArrayWriterTest extends \PHPUnit_Framework_TestCase
+class ArrayWriterTest extends TestCase
 {
-    /** @var  ArrayWriter $resource */
+    /** @var ArrayWriter $resource */
     private $resource;
-    
+
     /**
      * {@inheritdoc}
      */
@@ -29,10 +38,10 @@ class ArrayWriterTest extends \PHPUnit_Framework_TestCase
     {
         $testArray = [
             'level1' => [
-                'value 1', 'value 2', 'value 3'
-            ]
+                'value 1', 'value 2', 'value 3',
+            ],
         ];
-        
+
         $this->assertSame($testArray['level1'], $this->resource->getValue($testArray, '[level1]'));
     }
 
@@ -40,8 +49,8 @@ class ArrayWriterTest extends \PHPUnit_Framework_TestCase
     {
         $testArray = [
             'level1' => [
-                'value 1', 'value 2', 'value 3'
-            ]
+                'value 1', 'value 2', 'value 3',
+            ],
         ];
         $this->assertSame($testArray, $this->resource->getValue($testArray, ''));
     }
@@ -50,8 +59,8 @@ class ArrayWriterTest extends \PHPUnit_Framework_TestCase
     {
         $testArray = [
             'level1' => [
-                'value 1', 'value 2', 'value 3'
-            ]
+                'value 1', 'value 2', 'value 3',
+            ],
         ];
         $this->assertSame(true, $this->resource->isNode($testArray, '[level1]'));
     }
@@ -60,44 +69,44 @@ class ArrayWriterTest extends \PHPUnit_Framework_TestCase
     {
         $testArray = [
             'level1' => [
-                'value 1', 'value 2', 'value 3'
-            ]
+                'value 1', 'value 2', 'value 3',
+            ],
         ];
         $this->assertTrue($this->resource->isReadable($testArray, '[level1]'));
         $this->assertFalse($this->resource->isReadable($testArray, '[non-existent]'));
     }
-    
+
     public function testAdd()
     {
         $testArray = [
             'level1' => [
-                'value 1', 'value 2', 'value 3'
-            ]
-        ];
-        
-        $expected = [
-            'level1' => [
-                'value 1', 'value 2', 'value 3', 'ciao'
-            ]
-        ];
-        
-        $this->resource->add($testArray, '[level1]', 'ciao');
-        $this->assertSame($expected, $testArray);
-    }
-    
-    public function testAddCanCreateANewPropertyInTheRoot()
-    {
-        $testArray = [
-            'level1' => [
-                'value 1', 'value 2', 'value 3'
-            ]
+                'value 1', 'value 2', 'value 3',
+            ],
         ];
 
         $expected = [
             'level1' => [
-                'value 1', 'value 2', 'value 3'
+                'value 1', 'value 2', 'value 3', 'ciao',
             ],
-            'level2' => 'ciao'
+        ];
+
+        $this->resource->add($testArray, '[level1]', 'ciao');
+        $this->assertSame($expected, $testArray);
+    }
+
+    public function testAddCanCreateANewPropertyInTheRoot()
+    {
+        $testArray = [
+            'level1' => [
+                'value 1', 'value 2', 'value 3',
+            ],
+        ];
+
+        $expected = [
+            'level1' => [
+                'value 1', 'value 2', 'value 3',
+            ],
+            'level2' => 'ciao',
         ];
 
         $this->resource->add($testArray, '', 'ciao', 'level2');
@@ -108,18 +117,18 @@ class ArrayWriterTest extends \PHPUnit_Framework_TestCase
     {
         $testArray = [
             'level1' => [
-                'value 1', 'value 2', 'value 3'
-            ]
+                'value 1', 'value 2', 'value 3',
+            ],
         ];
-        
+
         $expected = [
             'level1' => [
                 0 => [
-                    'value 1', 'ciao'
+                    'value 1', 'ciao',
                 ],
                 'value 2',
-                'value 3'
-            ]
+                'value 3',
+            ],
         ];
 
         $this->resource->add($testArray, '[level1][0]', 'ciao');
@@ -130,19 +139,19 @@ class ArrayWriterTest extends \PHPUnit_Framework_TestCase
     {
         $testArray = [
             'level1' => [
-                'value 1', 'value 2', 'value 3'
-            ]
+                'value 1', 'value 2', 'value 3',
+            ],
         ];
-        
+
         $expected = [
             'level1' => [
                 0 => [
                     'OldValue' => 'value 1',
-                    'NewValue' => 'ciao'
+                    'NewValue' => 'ciao',
                 ],
                 'value 2',
-                'value 3'
-            ]
+                'value 3',
+            ],
         ];
 
         $this->resource->add($testArray, '[level1][0]', 'ciao', 'NewValue', 'OldValue');
@@ -153,17 +162,17 @@ class ArrayWriterTest extends \PHPUnit_Framework_TestCase
     {
         $testArray = [
             'level1' => [
-                'value 1', 'value 2', 'value 3'
-            ]
+                'value 1', 'value 2', 'value 3',
+            ],
         ];
-        
+
         $result = [
             'level1' => [
-                'value 1', 'value 2', 'value 3'
+                'value 1', 'value 2', 'value 3',
             ],
             'level2' => [
-                'value 1', 'value 2', 'value 3'
-            ]
+                'value 1', 'value 2', 'value 3',
+            ],
         ];
         $this->resource->cp($testArray, '[level1]', '[level2]');
         $this->assertSame($result, $testArray);
@@ -173,33 +182,33 @@ class ArrayWriterTest extends \PHPUnit_Framework_TestCase
     {
         $test = [
             'level1' => [
-                'value 1.1', 'value 1.2', 'value 1.3'
+                'value 1.1', 'value 1.2', 'value 1.3',
             ],
             'level2' => [
-                'key1' => 'value 2.1', 'value 2.2', 'value 2.3'
-            ]
+                'key1' => 'value 2.1', 'value 2.2', 'value 2.3',
+            ],
         ];
         $result = [
             'level1' => [
-                'value 1.1', 'value 1.2', 'value 1.3'
+                'value 1.1', 'value 1.2', 'value 1.3',
             ],
             'level2' => [
-                'key1' => 'value 2.1', 'value 2.2', 'value 2.3'
+                'key1' => 'value 2.1', 'value 2.2', 'value 2.3',
             ],
             'key1' => 'value 2.1',
             'value 2.2',
-            'value 2.3'
+            'value 2.3',
         ];
         $this->resource->cp($test, '[level2]', '[]');
         $this->assertSame($result, $test);
     }
-    
+
     public function testCpThrowsExceptionIfFromIsNotReadable()
     {
         $testArray = [
             'level1' => [
-                'value 1', 'value 2', 'value 3'
-            ]
+                'value 1', 'value 2', 'value 3',
+            ],
         ];
         $this->expectException(AccessException::class);
         $this->resource->cp($testArray, '[not-known]', '[not-known2]');
@@ -209,21 +218,21 @@ class ArrayWriterTest extends \PHPUnit_Framework_TestCase
     {
         $testArray = [
             'level1' => [
-                'value 1', 'value 2', 'value 3'
-            ]
+                'value 1', 'value 2', 'value 3',
+            ],
         ];
         $result = [
             'level1' => [
-                'value 1', 'value 2', 'value 3'
+                'value 1', 'value 2', 'value 3',
             ],
             'level2' => [
-                'value 1', 'value 2', 'value 3'
-            ]
+                'value 1', 'value 2', 'value 3',
+            ],
         ];
         $this->resource->cpSafe($testArray, '[level1]', '[level3]');
         $this->assertSame($result['level1'], $testArray['level3']);
     }
-    
+
     /**
      * This exception is not testable as the isWrite method ever returns true.
      */
@@ -231,11 +240,11 @@ class ArrayWriterTest extends \PHPUnit_Framework_TestCase
     {
         $test = [
             'level1' => [
-                'value 1.1', 'value 1.2', 'value 1.3'
+                'value 1.1', 'value 1.2', 'value 1.3',
             ],
             'level2' => [
-                'value 2.1', 'value 2.2', 'value 2.3'
-            ]
+                'value 2.1', 'value 2.2', 'value 2.3',
+            ],
         ];
         $this->expectException(AccessException::class);
         $this->resource->cpSafe($test, '[level1]', '[level2]');
@@ -245,13 +254,13 @@ class ArrayWriterTest extends \PHPUnit_Framework_TestCase
     {
         $test = [
             'level1' => [
-                'value 1.1', 'value 1.2', 'value 1.3'
-            ]
+                'value 1.1', 'value 1.2', 'value 1.3',
+            ],
         ];
         $result = [
-            'value 2.1', 'value 2.2', 'value 2.3'
+            'value 2.1', 'value 2.2', 'value 2.3',
         ];
-        
+
         $this->resource->edit($test, '[level1]', $result);
         $this->assertSame($test['level1'], $result);
     }
@@ -260,11 +269,11 @@ class ArrayWriterTest extends \PHPUnit_Framework_TestCase
     {
         $test = [
             'level1' => [
-                'value 1.1', 'value 1.2', 'value 1.3'
-            ]
+                'value 1.1', 'value 1.2', 'value 1.3',
+            ],
         ];
         $result = [
-            'value 2.1', 'value 2.2', 'value 2.3'
+            'value 2.1', 'value 2.2', 'value 2.3',
         ];
 
         $this->resource->edit($test, '[]', $result);
@@ -275,30 +284,30 @@ class ArrayWriterTest extends \PHPUnit_Framework_TestCase
     {
         $testArray = [
             'level1' => [
-                'value 1', 'value 2', 'value 3'
-            ]
+                'value 1', 'value 2', 'value 3',
+            ],
         ];
         $this->expectException(AccessException::class);
         $this->resource->edit($testArray, '[non-existent]', ['value 1', 'value 2']);
     }
-    
+
     public function testMerge()
     {
         $test = [
             'level1' => [
-                'value 1.1', 'value 1.2', 'value 1.3'
+                'value 1.1', 'value 1.2', 'value 1.3',
             ],
             'level2' => [
-                'key1' => 'value 2.1', 'value 2.2', 'value 2.3'
-            ]
+                'key1' => 'value 2.1', 'value 2.2', 'value 2.3',
+            ],
         ];
         $result = [
             'level1' => [
-                'value 1.1', 'value 1.2', 'value 1.3'
+                'value 1.1', 'value 1.2', 'value 1.3',
             ],
             'key1' => 'value 2.1',
             'value 2.2',
-            'value 2.3'
+            'value 2.3',
         ];
         $this->resource->merge($test, '[level2]', '[]');
         $this->assertSame($test, $result);
@@ -308,31 +317,31 @@ class ArrayWriterTest extends \PHPUnit_Framework_TestCase
     {
         $test = [
             'level1' => [
-                'value 1.1', 'value 1.2', 'value 1.3'
+                'value 1.1', 'value 1.2', 'value 1.3',
             ],
-            'level2' => 'value 2.1'
+            'level2' => 'value 2.1',
         ];
         $result = [
             'level1' => [
-                'value 1.1', 'value 1.2', 'value 1.3'
+                'value 1.1', 'value 1.2', 'value 1.3',
             ],
-            'value 2.1'
+            'value 2.1',
         ];
         $this->resource->merge($test, '[level2]', '[]');
         $this->assertSame($test, $result);
     }
-    
+
     public function testMv()
     {
         $testArray = [
             'level1' => [
-                'value 1', 'value 2', 'value 3'
-            ]
+                'value 1', 'value 2', 'value 3',
+            ],
         ];
         $result = [
             'level2' => [
-                'value 1', 'value 2', 'value 3'
-            ]
+                'value 1', 'value 2', 'value 3',
+            ],
         ];
         $this->resource->mv($testArray, '[level1]', '[level2]');
         $this->assertSame($result, $testArray);
@@ -342,19 +351,19 @@ class ArrayWriterTest extends \PHPUnit_Framework_TestCase
     {
         $test = [
             'level1' => [
-                'value 1.1', 'value 1.2', 'value 1.3'
+                'value 1.1', 'value 1.2', 'value 1.3',
             ],
             'level2' => [
-                'key1' => 'value 2.1', 'value 2.2', 'value 2.3'
-            ]
+                'key1' => 'value 2.1', 'value 2.2', 'value 2.3',
+            ],
         ];
         $result = [
             'level1' => [
-                'value 1.1', 'value 1.2', 'value 1.3'
+                'value 1.1', 'value 1.2', 'value 1.3',
             ],
             'key1' => 'value 2.1',
             'value 2.2',
-            'value 2.3'
+            'value 2.3',
         ];
         $this->resource->mv($test, '[level2]', '[]');
         $this->assertSame($result, $test);
@@ -364,13 +373,13 @@ class ArrayWriterTest extends \PHPUnit_Framework_TestCase
     {
         $testArray = [
             'level1' => [
-                'value 1', 'value 2', 'value 3'
-            ]
+                'value 1', 'value 2', 'value 3',
+            ],
         ];
         $result = [
             'level2' => [
-                'value 1', 'value 2', 'value 3'
-            ]
+                'value 1', 'value 2', 'value 3',
+            ],
         ];
         $this->resource->mvSafe($testArray, '[level1]', '[level2]');
         $this->assertSame($result, $testArray);
@@ -380,11 +389,11 @@ class ArrayWriterTest extends \PHPUnit_Framework_TestCase
     {
         $test = [
             'level1' => [
-                'value 1.1', 'value 1.2', 'value 1.3'
+                'value 1.1', 'value 1.2', 'value 1.3',
             ],
             'level2' => [
-                'value 2.1', 'value 2.2', 'value 2.3'
-            ]
+                'value 2.1', 'value 2.2', 'value 2.3',
+            ],
         ];
         $this->expectException(AccessException::class);
         $this->resource->mvSafe($test, '[level1]', '[level2]');
@@ -394,19 +403,19 @@ class ArrayWriterTest extends \PHPUnit_Framework_TestCase
     {
         $test = [
             'level1' => [
-                'value 1.1', 'value 1.2', 'value 1.3'
+                'value 1.1', 'value 1.2', 'value 1.3',
             ],
             'level2' => [
-                'key1' => 'value 2.1', 'value 2.2', 'value 2.3'
-            ]
+                'key1' => 'value 2.1', 'value 2.2', 'value 2.3',
+            ],
         ];
         $result = [
             'level1' => [
-                'value 1.1', 'value 1.2', 'value 1.3'
+                'value 1.1', 'value 1.2', 'value 1.3',
             ],
             'key1' => 'value 2.1',
             'value 2.2',
-            'value 2.3'
+            'value 2.3',
         ];
         $this->resource->mvUp($test, '[level2]');
         $this->assertSame($test, $result);
@@ -416,48 +425,48 @@ class ArrayWriterTest extends \PHPUnit_Framework_TestCase
     {
         $test = [
             'level1' => [
-                'value 1.1', 'value 1.2', 'value 1.3'
+                'value 1.1', 'value 1.2', 'value 1.3',
             ],
-            'level2' => 'value 2.1'
+            'level2' => 'value 2.1',
         ];
         $result = [
             'level1' => [
-                'value 1.1', 'value 1.2', 'value 1.3'
+                'value 1.1', 'value 1.2', 'value 1.3',
             ],
-            'value 2.1'
+            'value 2.1',
         ];
         $this->resource->mvUp($test, '[level2]');
         $this->assertSame($test, $result);
     }
-    
+
     public function testRm()
     {
         $testArray = [
             'level1' => [
-                'value 1', 'value 2', 'value 3'
-            ]
+                'value 1', 'value 2', 'value 3',
+            ],
         ];
         $result = ['level1' => ['value 1', 'value 2']];
         $this->resource->rm($testArray, '[level1][2]');
         $this->assertSame($result, $testArray);
     }
-    
+
     public function testWrap()
     {
         $testArray = [
             'level1' => [
-                'value 1', 'value 2', 'value 3'
-            ]
+                'value 1', 'value 2', 'value 3',
+            ],
         ];
-        
+
         $result = [
             'level1' => [
                 'wrapper' => [
-                    'value 1', 'value 2', 'value 3'
-                ]
-            ]
+                    'value 1', 'value 2', 'value 3',
+                ],
+            ],
         ];
-        
+
         $this->resource->wrap($testArray, '[level1]', 'wrapper');
         $this->assertSame($result, $testArray);
     }
@@ -466,29 +475,29 @@ class ArrayWriterTest extends \PHPUnit_Framework_TestCase
     {
         $testArray = [
             'level1' => [
-                'value 1', 'value 2', 'value 3'
-            ]
+                'value 1', 'value 2', 'value 3',
+            ],
         ];
 
         $result = [
             'wrapper' => [
                 'level1' => [
-                    'value 1', 'value 2', 'value 3'
-                ]
-            ]
+                    'value 1', 'value 2', 'value 3',
+                ],
+            ],
         ];
 
         $this->resource->wrap($testArray, '', 'wrapper');
         $this->assertSame($result, $testArray);
     }
-    
+
     public function testPathize()
     {
-        $test = 'path';
+        $test     = 'path';
         $expected = '[path]';
-        
+
         $result = ArrayWriter::pathize($test);
-        
+
         $this->assertSame($expected, $result);
     }
 }
